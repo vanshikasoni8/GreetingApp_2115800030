a215219676bf440745cade8608ecad007fe17a29
Vanshika soni <106619869+vanshikasoni8@users.noreply.github.com>
2025-03-04 15:33:00 +0530

    fixed changes

--- BussinessLayer/obj/Debug/net6.0/BussinessLayer.AssemblyInfo.cs
+++ BussinessLayer/obj/Debug/net6.0/BussinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BussinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb98d801f4fc57d407ece3bf7c89faa08553d1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BussinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BussinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
